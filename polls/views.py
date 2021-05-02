8e8fb3c7d75869319745999395c90dbe52f8c9c2
omidgholami88 <omidgholami88@gmail.com>
2021-05-02 19:22:23 +0430

    using mixins

--- polls/views.py
+++ polls/views.py
@@ -2,53 +2,39 @@ from django.shortcuts import render
 
 from .models import Question
 from .serializers import QuestionSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import serializers, status
+from rest_framework import mixins, serializers
+from rest_framework import generics
 
 
-class QuestionList(APIView):
+class QuestionList(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   generics.GenericAPIView):
     """list all questions or generate new one
     """
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
 
-    def get(self, request, format=None):
-        questions = Question.objects.all()
-        serializers = QuestionSerializer(questions, many=True)
-        return Response(serializers.data)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def post(self, request, format):
-        serializer = QuestionSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class QuestionDetail(APIView):
+class QuestionDetail(mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     generics.GenericAPIView):
     """retrieve, update or delete an instance
     """
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
 
-    def get_object(self, pk):
-        try:
-            return Question.objects.get(pk=pk)
-        except Question.DoesNotExist:
-            raise Http404
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def get(self, request, pk, format=None):
-        question = self.get_object(pk)
-        serializer = QuestionSerializer(question)
-        return Response(serializer.data)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def put(self, request, pk, format=None):
-        question = self.get_object(pk)
-        serializer = QuestionSerializer(question, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        question = self.get_object(pk)
-        question.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
