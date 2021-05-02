6401095078e801a1873ce7421c245be98948a27c
omidgholami88 <omidgholami88@gmail.com>
2021-05-02 22:38:26 +0430

    using generics

--- polls/views.py
+++ polls/views.py
@@ -6,35 +6,15 @@ from rest_framework import mixins, serializers
 from rest_framework import generics
 
 
-class QuestionList(mixins.ListModelMixin,
-                   mixins.CreateModelMixin,
-                   generics.GenericAPIView):
+class QuestionList(generics.ListCreateAPIView):
     """list all questions or generate new one
     """
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class QuestionDetail(mixins.RetrieveModelMixin,
-                     mixins.UpdateModelMixin,
-                     mixins.DestroyModelMixin,
-                     generics.GenericAPIView):
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
     """retrieve, update or delete an instance
     """
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
