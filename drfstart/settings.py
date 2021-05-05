9073eefdd5f6c8f3ed75eff06de8e8bb97eee0ee
omidgholami88 <omidgholami88@gmail.com>
2021-05-05 11:56:46 +0430

    using drf-spectacular

--- drfstart/settings.py
+++ drfstart/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'polls',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -128,5 +129,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'DRF start',
+    'DESCRIPTION': 'API with DRF',
+    'VERSION': '1.0.0',
 }
--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 from . import views
 
@@ -12,4 +13,9 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('choices/', views.ChoiceList.as_view(), name='choice-list'),
     path('choices/<int:pk>', views.ChoiceDetail.as_view(), name='choice-detail'),
+    # YOUR PATTERNS
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
